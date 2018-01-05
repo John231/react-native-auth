e0aa43813ba2baa3480396e8d292c7ac472172bd
John231 <j_b_93@hotmail.co.uk>
2018-01-05 10:47:30 +0100

    Lesson 64 - Creating a reusable Input component

---
+++ src/components/common/Input.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { TextInput, View, Text } from "react-native";
+
+const Input = ({ label }) => {
+  return (
+    <View>
+      <Text>{label}</Text>
+    </View>
+  );
+};
+export { Input };
