8668821696474ef666fa53cc466c005943e35350
John231 <j_b_93@hotmail.co.uk>
2018-01-05 10:12:01 +0100

    Lesson 59 - Completed firebase setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import firebase from "firebase";
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { Header } from "./components/common";
 
 class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    firebase.initializeApp({
+      apiKey: "AIzaSyBcGI5__NFNfowzaJG342Hgo1tfN3HWYzY",
+      authDomain: "authentication-78410.firebaseapp.com",
+      databaseURL: "https://authentication-78410.firebaseio.com",
+      projectId: "authentication-78410",
+      storageBucket: "authentication-78410.appspot.com",
+      messagingSenderId: "227583138901"
+    });
+  }
   render() {
     return (
       <View>
